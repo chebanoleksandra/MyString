fec2f9632f0865d437470f6944fce3ef8ccc0a23
Oleksandra <alexa30che@gmail.com>
2024-10-04 14:44:17 +0300

    add MyStrcpy(), MyStrLen(), MyStrCmp()

--- myString/myString.cpp
+++ myString/myString.cpp
@@ -11,6 +11,13 @@ public:
 	MyString(const MyString& obj);
 	void Print();
 	void Input();
+	void MyStrcpy(MyString& obj);
+	bool MyStrStr(const char* str);
+	int  MyChr(char c);
+	int MyStrLen();
+	void MyStrCat(MyString& b);
+	void MyDelChr(char c);
+	int MyStrCmp(MyString& b);
 	~MyString();
 };
 
@@ -53,6 +60,37 @@ void MyString::Input()
 	strcpy_s(str, length + 1, buff);
 }
 
+void MyString::MyStrcpy(MyString& obj)
+{
+	if (str != nullptr)
+	{
+		delete[] str;
+	}
+	length = obj.length;
+	str = new char[length + 1];
+	strcpy_s(str, length + 1, obj.str);
+}
+
+int MyString::MyStrLen()
+{
+	return length;
+}
+
+int MyString::MyStrCmp(MyString& b)
+{
+	if ((*this).length < b.length)
+	{
+		return -1;
+	}
+	else if ((*this).length > b.length)
+	{
+		return 1;
+	}
+	else {
+		return 0;
+	}
+}
+
 MyString::~MyString()
 {
 	delete[] str;
@@ -68,5 +106,8 @@ int main()
 
 	obj2.Input();
 	obj2.Print();
+	cout << obj1.MyStrCmp(obj2) << endl;
+	obj1.MyStrcpy(obj2);
+	obj1.Print();
 }
 
